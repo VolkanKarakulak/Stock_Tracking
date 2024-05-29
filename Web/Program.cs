1aeff1b3f5b83d4ea79571a374aa39a5a6aa3236
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-05-30 00:26:58 +0300

    product, category modelleri eklendi, veritabanı oluşturuldu

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,12 @@
+using DataAccess.Concrete.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Stock_TrackingDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
