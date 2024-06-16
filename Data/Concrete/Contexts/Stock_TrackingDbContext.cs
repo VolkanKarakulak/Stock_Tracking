c2f8930b5e18deb9a38c7366bbfd8e248c8521b0
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-06-16 19:13:01 +0300

    Genel değişiklikler yapıldı

--- Data/Concrete/Contexts/Stock_TrackingDbContext.cs
+++ Data/Concrete/Contexts/Stock_TrackingDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Entity.Concrete;
+﻿using Entity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
---
+++ Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public Category Category { get; set; }
+       
+
+    }
+}
