39194f4b0e0cafedc04419669620f297d1ab4714
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-06-16 15:13:51 +0300

    Proje .net 8 versiyonuna güncellendi, katmanlar değiştirildi

---
+++ Data/Concrete/Contexts/Stock_TrackingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Concrete.Contexts
+{
+    public class Stock_TrackingDbContext : DbContext
+    {
+        public Stock_TrackingDbContext(DbContextOptions<Stock_TrackingDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+
+    }
+}
