342a67c3d47387bc981e2455365f4ffbd88c5d7c
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-06-18 14:08:08 +0300

    Base, Category, Product Dto'ları eklendi

--- Data/Abstract/IGenericRepository.cs
+++ Data/Abstract/IGenericRepository.cs
@@ -13,7 +13,7 @@ namespace Data.Abstract
         IQueryable<T> GetAll();
         IQueryable<T> Where(Expression<Func<T, bool>> expression); // where ile veritabanına yapışacak olan sorgu oluşturuluyor, sorgu yapılmıyor
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task CreateRangeAsync(IEnumerable<T> entities); // birden fazla kayıt, 
+        Task CreateRangeAsync(IEnumerable<T> entities); // birden fazla kayıt
         Task CreateAsync(T entity);
         void Update(T entity); //void de olabilir, çünkü update/delete uzun süren işlemler değil
         void Delete(T entity); // void de olabilir
