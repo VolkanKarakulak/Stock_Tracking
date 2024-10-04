9848a3ab54748acd26bc130fb781dea0b00d7ef2
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-10-04 10:31:08 +0300

    User entitysinin oluştrulması

--- Data/Context/Stock_TrackingDbContext.cs
+++ Data/Context/Stock_TrackingDbContext.cs
@@ -12,6 +12,7 @@ namespace Data.Contexts
 
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Product>? Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ Data/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class User : BaseEntity
+    {
+        [StringLength(50), Column(TypeName = "Varchar(50)")]
+        public string Fullname { get; set; }
+
+        [StringLength(100), Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nText"), DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime LastLoginDate { get; set; }
+
+        [StringLength(20), Column(TypeName = "Varchar(20)")]
+        public string LastIPAddress { get; set; }
+
+        public string SaltPassword { get; set; }
+        //public UserRefreshToken UserRefreshToken { get; set; }
+        public int? RoleId { get; set; }
+        //public Role Role { get; set; }
+    }
+}
