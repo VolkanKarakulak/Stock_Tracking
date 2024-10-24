3c3fa24b38e5fa06201878e5353145caedde6c8e
Volkan Karakulak <volkan.karakulakk@gmail.com>
2024-10-24 11:53:37 +0300

    Category Service yazılacak
    https://volkankarakulakk-1727687394263.atlassian.net/browse/ST-13

---
+++ Service/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Data.Entities;
+using Data.Repositories.GenericRepositories;
+using Data.UnitOfWorks;
+using Service.DTOs.PaginationDto;
+using Service.DTOs.ResponseDto;
+using Service.Services.GenericService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.CategoryService
+{
+    public class CategoryService : GenericService<Category>, ICategoryService
+    {
+        public CategoryService(IGenericRepository<Category> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
